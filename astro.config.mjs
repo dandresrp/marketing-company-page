c0d82f891145d601dfb5f912e67ae0dc4fb03ea2
Diego Rios <73723371+dandresrp@users.noreply.github.com>
2025-12-15 16:46:24 -0600

    Update base path in Astro config for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dandresrp.github.io',
-  base: '/',
+  base: '/marketing-company-page',
   integrations: [tailwind({
     applyBaseStyles: true,
   })]
